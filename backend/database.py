2602b1ef5b01abe316012e0f7e4d3e449b701754
Florian Reiter <fre@macbook-pro-von-florian.bumblebee-discus.ts.net>
2025-09-19 12:47:18 +0200

    airtables fix

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,16 @@
 from pyairtable import Api
 import os
 
-AIRTABLE_API_TOKEN = os.environ.get("AIRTABLE_API_TOKEN")
-AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
-AIRTABLE_TABLE_ID = os.environ.get("AIRTABLE_TABLE_ID")
-
-api = Api(AIRTABLE_API_TOKEN)
-table = api.table(AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID)
+def get_table():
+    AIRTABLE_API_TOKEN = os.environ.get("AIRTABLE_API_TOKEN")
+    AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
+    AIRTABLE_TABLE_ID = os.environ.get("AIRTABLE_TABLE_ID")
+    api = Api(AIRTABLE_API_TOKEN)
+    table = api.table(AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID)
+    return table
 
 def get_all_coffees():
+    table = get_table()
     records = table.all()
     coffees = []
     for record in records:
@@ -24,12 +26,14 @@ def get_all_coffees():
     return coffees
 
 def increment_coffee_vote(coffee_id):
+    table = get_table()
     record = table.get(coffee_id)
     votes = record["fields"].get("votes", 0)
     table.update(coffee_id, {"votes": votes + 1})
     return True
 
 def add_coffee(name, description, image_url):
+    table = get_table()
     record = table.create(
         {
             "name": name,
@@ -41,10 +45,12 @@ def add_coffee(name, description, image_url):
     return record["id"]
 
 def delete_coffee(coffee_id):
+    table = get_table()
     table.delete(coffee_id)
     return True
 
 def update_coffee(coffee_id, name, description, image_url):
+    table = get_table()
     table.update(
         coffee_id,
         {
